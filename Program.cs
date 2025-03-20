b75439052feefb83b443da36c90dc7cd2cdc7cd1
CrystalW11 <crystal.warmack@gmail.com>
2025-03-20 12:16:53 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, C#!");
+
+
+// Rolling dice for  a random number 
+Random dice = new Random();
+int roll = dice.Next(1, 7);
+Console.WriteLine(roll);
+
