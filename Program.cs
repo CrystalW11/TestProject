537baf9a60ce6abb7346f169bd07a8400a241f8e
CrystalW11 <crystal.warmack@gmail.com>
2025-03-20 12:29:14 -0600

    updated folders

--- Program.cs
+++ Program.cs
@@ -3,7 +3,10 @@ Console.WriteLine("Hello, C#!");
 
 
 // Rolling dice for  a random number 
-Random dice = new Random();
-int roll = dice.Next(1, 7);
-Console.WriteLine(roll);
+// Random dice = new();
+// int roll = dice.Next(1, 7);
+// Console.WriteLine(roll);
 
+Random dice = new Random();
+
+int roll = dice.Next();
